2117e82f37088b3175b9ecdcead681a216a3c6e7
CodingDaniel1 <cuberdanielchen@gmail.com>
2024-06-14 22:48:43 +0800

    First init

---
+++ Unity.Scenes/BuildManifest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Unity.Scenes
+{
+    public struct BuildManifest
+    {
+        internal const int ManifestCurrentVersion = 1;
+
+        public string RootPath => Path.Value;
+
+        public string EntityScenePath => $"{RootPath}\\EntityScenes";
+
+        [NonSerialized] public FixedString512Bytes Path;
+
+        public BlobArray<Hash128> Scenes;
+    }
+}
