1b67d4f867e3395743e50c237f78a1d778258a2c
CodingDaniel1 <cuberdanielchen@gmail.com>
2024-09-10 23:43:04 +0800

    Update AssemblyInfo.cs

--- Unity.Entities.Editor/AssemblyInfo.cs
+++ Unity.Entities.Editor/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.Entities.Editor.PerformanceTests")]
 [assembly: InternalsVisibleTo("Unity.NetCode.Editor")]
 [assembly: InternalsVisibleTo("Unity.Physics.Editor")]
+[assembly: InternalsVisibleTo("SqualiveNetworking.ArmAlive.Hybrid.Editor.Server")]
